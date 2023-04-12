8242d23816f246391734afbd0a368a7dd14a5688
mrmitcp1 <mrmitcp55@gmail.com>
2023-04-12 10:43:38 +0700

    kiemthu

--- app.ts
+++ app.ts
@@ -1,5 +1,10 @@
 export class AbsoluteNumberCalculator {
     public static findAbsolute(num: number): number {
-        return 0;
+        //throw new Error("Unsupported Operation Exception");
+        //return 0;
+
+        if(num < 0)
+            return  -num;
+        return num;
     }
 }
